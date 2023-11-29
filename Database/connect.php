b7b33e23bcadcc598d47dd35aa0aa183bb43490a
Bruno Pontes <bruno_confessor@hotmail.com>
2023-11-29 20:11:25 -0300

    refatorado a conexão

--- Database/connect.php
+++ Database/connect.php
@@ -1,14 +1,21 @@
 <?php
 
-require_once 'config.php';
+class Connect {
+    public $hostname = 'mysql:host=127.0.0.2;';
+    public $useranme = 'root';
+    public $password = '901229';
+    public $dbname = 'dbname=Portal';
 
-$config = new Config();
+    var $conn;
 
-try {
-    $connect_bank = new PDO($config->hostname . $config->dbname, $config->useranme, $config->password);
-} catch (PDOException $objErro) {
-    echo 'SGBD Indisponivel: ' . $objErro -> getMessage();
-    exit();
+    function Connection(){
+        try {
+            $this->conn = new PDO($this->hostname . $this->dbname, $this->useranme, $this->password);
+        } catch (PDOException $objErro) {
+            echo 'SGBD Indisponivel: ' . $objErro -> getMessage();
+            exit();
+        }
+    }
 }
 
 
