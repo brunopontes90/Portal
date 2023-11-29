2b4ff9fd641aacf0540fcd8074d04e2132d74771
Bruno Pontes <bruno_confessor@hotmail.com>
2023-11-29 20:24:43 -0300

    Refatorando banco de dados e query para login

--- Database/connect.php
+++ Database/connect.php
@@ -8,12 +8,39 @@ class Connect {
 
     var $conn;
 
-    function Connection(){
+    public function Connection(){
         try {
             $this->conn = new PDO($this->hostname . $this->dbname, $this->useranme, $this->password);
         } catch (PDOException $objErro) {
             echo 'SGBD Indisponivel: ' . $objErro -> getMessage();
             exit();
+        }
+    }
+
+    public function Login(){
+        $query = $this->conn->prepare("SELECT ID, Nome, EAdmin FROM Pessoas WHERE Email = :email AND Senha = :senha");
+        $query->bindParam(':email', $_POST['login']);
+        $query->bindParam(':senha', $_POST['senha']);
+        $query->execute();
+
+        $result = $query->fetchAll(PDO::FETCH_CLASS);
+
+        if (count($result) == 1) {
+            session_start();
+    
+            //Cria vetor no SESSION para o login do user e verifica se existe esse login nas outras paginas
+            $_SESSION['login'] = $result[0]->Nome;
+            $_SESSION['ID'] = $result[0]->ID;
+            $_SESSION['EAdmin'] = $result[0]->EAdmin;
+    
+            if($_SESSION['EAdmin'] = $result[0]->EAdmin == 1){
+                header('Location: ../Pages/admin/admin.php');
+            }else {
+                header('Location: ../Pages/user/user.php'); 
+            }
+    
+        }else{
+            header('Location: ../index.php'); //se nao estiver entra no form
         }
     }
 }
