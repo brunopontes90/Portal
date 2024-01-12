a74ab235ba3f229c40af9644411c37062d467d6b
Bruno Pontes <bruno_confessor@hotmail.com>
2024-01-12 15:13:37 -0300

    Criado funcao para deletar

--- Backend/delete.php
+++ Backend/delete.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
-    include_once __DIR__ . '/../../Database/querys/delete.php';
+    include_once __DIR__ . '/../Database/querys/delete.php';
     $delete_user = new Delete();
     $delete_data = $delete_user->DeleteQuery($_GET['ID']);
 
-?>
